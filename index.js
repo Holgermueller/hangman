ed79099208ec0096d3eb9eee8f1d68b6c2ce6326
holgermueller <9jholger@gmail.com>
2023-05-03 19:22:57 -0500

    got keypress to log

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 "use strict";
 
-const word = "hello";
+document.addEventListener("keydown", getPlayerGuess);
 
+const word = "hello";
 let lettersGuessed = ["letters guessed to here"];
 
 function pushWordToDOM() {
-  document.getElementById("wordHolder").innerHTML = word;
+  let underscoredWord = word.replace(/[a-z]/g, "-");
+  document.getElementById("wordHolder").innerHTML = underscoredWord;
+}
+
+function guessesLeftToDom() {
+  let guessesLeft = word.length;
+  document.getElementById("guessesLeft").innerHTML = guessesLeft;
 }
 
 function storeGuessedLetters() {
   document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
 }
 
+function getPlayerGuess(e) {
+  let lettersGuessed = e.key;
+  console.log(lettersGuessed);
+}
+
 pushWordToDOM();
 storeGuessedLetters();
+guessesLeftToDom();
+getPlayerGuess();
