5da76c8f7683a55cb2747efd9a101d89ed95bf88
holgermueller <9jholger@gmail.com>
2023-05-06 13:50:00 -0500

    got letter buttons on dom

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 "use strict";
 
-document.addEventListener("keydown", getPlayerGuess);
-
 const word = "hello";
 let lettersGuessed = ["letters guessed to here"];
+
+function generateButtons() {
+  let buttonLetters = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      (letter) =>
+        `
+    <button
+    class="btn btn-lg btn-primary m-2"
+    id='` +
+        letter +
+        `'
+    onClick="handleGuess('` +
+        letter +
+        `')"
+    >
+    ` +
+        letter +
+        `
+    </button>
+    `
+    )
+    .join("");
+
+  document.getElementById("keyboard").innerHTML = buttonLetters;
+}
 
 function pushWordToDOM() {
   let underscoredWord = word.replace(/[a-z]/g, "-");
   document.getElementById("wordHolder").innerHTML = underscoredWord;
 }
 
+function handleGuess(chosenLetter) {
+  console.log("click");
+}
+
 function guessesLeftToDom() {
   let guessesLeft = word.length;
-  document.getElementById("guessesLeft").innerHTML = guessesLeft;
+  document.getElementById("guessesLeft").innerHTML = guessesLeft + 1;
 }
 
 function storeGuessedLetters() {
   document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
 }
 
-function getPlayerGuess(e) {
-  let lettersGuessed = e.key;
-  console.log(lettersGuessed);
-}
-
 pushWordToDOM();
 storeGuessedLetters();
 guessesLeftToDom();
-getPlayerGuess();
+generateButtons();
